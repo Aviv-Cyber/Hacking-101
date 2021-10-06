6583c063208a1b921ebaeabb5a28e97b864b5dc1
Yehi <cyberisrael.red@gmail.com>
2021-10-06 14:54:30 +0300

    6/10 update

---
+++ .JS/main.js
@@ -0,0 +1,1 @@
+document.querySelector("#SPFieldFile > a").click()
--- .Py/Test.py
+++ .Py/Test.py
@@ -1,1 +1,6 @@
-print("hello")
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\avivy\GitHub\Hacking 101\.CSV\Paths.csv", header=None)
+A_col = df.iloc[:, 0].values
+for i in range(186):
+    print("start " + a + str(i) + "\n" + "timeout /t 5")
